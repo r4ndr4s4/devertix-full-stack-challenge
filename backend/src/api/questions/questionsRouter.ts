ad45b23c7f1caef458aa238ca98d07a719259650
András Rakita <Rakita.Andras@gmail.com>
2024-07-04 15:20:11 +0200

    feat(backend): adding questions and sending them as a response

--- backend/src/api/questions/questionsRouter.ts
+++ backend/src/api/questions/questionsRouter.ts
@@ -1,13 +1,19 @@
 import express, { Response } from 'express';
+import asyncHandler from 'express-async-handler';
 
 import { logger } from '@/server';
 
 const router = express.Router();
 
-router.get('/', (_, res: Response) => {
-  logger.info('/questions endpoint called');
+router.get(
+  '/',
+  asyncHandler(async (_, res: Response) => {
+    logger.info('/questions endpoint called');
 
-  res.sendStatus(200);
-});
+    const { default: questions } = await import('@/api/questions/questions.json');
+
+    res.json({ data: questions });
+  })
+);
 
 export default router;
