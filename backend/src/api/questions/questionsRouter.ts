25f7fd43aeee8b4074bc307452c5e2b793f0c360
András Rakita <Rakita.Andras@gmail.com>
2024-07-04 16:25:05 +0200

    feat(backend): questions number query param logic

--- backend/src/api/questions/questionsRouter.ts
+++ backend/src/api/questions/questionsRouter.ts
@@ -1,5 +1,7 @@
 import express, { Response } from 'express';
 import asyncHandler from 'express-async-handler';
+import { z } from 'zod';
+import { validateRequest } from 'zod-express-middleware';
 
 import { logger } from '@/server';
 
@@ -7,12 +9,35 @@ const router = express.Router();
 
 router.get(
   '/',
-  asyncHandler(async (_, res: Response) => {
+  validateRequest({
+    query: z.object({
+      number: z.coerce.number(),
+    }),
+  }),
+  asyncHandler(async ({ query: { number: requestedQuestions } }, res: Response) => {
     logger.info('/questions endpoint called');
 
-    const { default: questions } = await import('@/api/questions/questions.json');
+    const {
+      default: { questions },
+    } = await import('@/api/questions/questions.json');
+    const availableQuestions = questions.length;
 
-    res.json({ data: questions });
+    const requestedNumberOfQuestions = Number(requestedQuestions);
+    logger.info({ availableQuestions, requestedQuestions, requestedNumberOfQuestions }); // TODO remove
+
+    if (requestedNumberOfQuestions === 0) {
+      throw new Error('Invalid requested question number (0).');
+    }
+
+    if (requestedNumberOfQuestions > availableQuestions) {
+      throw new Error(`Invalid requested question number (only ${availableQuestions} available).`);
+    }
+
+    res.json({
+      data: {
+        questions: questions.slice(0, requestedNumberOfQuestions),
+      },
+    });
   })
 );
 
