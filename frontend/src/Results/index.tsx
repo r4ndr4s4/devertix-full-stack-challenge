fd082e4917464130c6289142b4880440012a503c
András Rakita <Rakita.Andras@gmail.com>
2024-07-05 16:07:45 +0200

    feat(frontend): answer list in result component

--- frontend/src/Results/index.tsx
+++ frontend/src/Results/index.tsx
@@ -1,36 +1,58 @@
-import { useContext } from "react";
+import { useCallback, useContext, useMemo } from "react";
 import { AppStateContext } from "../App/AppState";
 import { IQuestionsWithAnswers } from "../App/types";
+import { NUMBER_OF_QUESTIONS } from "../App";
 
 function Results() {
   const { questions, setQuestions, setCurrentQuestion } =
     useContext(AppStateContext);
 
+  const correctAnswers = useMemo(
+    () =>
+      questions.filter(({ answer, currentAnswer }) => answer === currentAnswer)
+        .length,
+    [questions]
+  );
+
+  const answerList = useMemo(
+    () =>
+      questions.map(({ question, answer, currentAnswer }) => {
+        return (
+          <p key={crypto.randomUUID()}>{`${
+            answer === currentAnswer ? "+" : "-"
+          } ${question}`}</p>
+        );
+      }),
+    [questions]
+  );
+
+  const resetGame = useCallback(() => {
+    const currentQuestions = [...questions];
+
+    const questionsWithAnswers: IQuestionsWithAnswers = currentQuestions.map(
+      (question) => {
+        return {
+          ...question,
+          currentAnswer: undefined,
+        };
+      }
+    );
+
+    setQuestions(questionsWithAnswers);
+
+    setCurrentQuestion(0);
+  }, [questions, setCurrentQuestion, setQuestions]);
+
   return (
     <>
-      <h1>Results</h1>
+      <h1>
+        You scored
+        <br />({`${correctAnswers}/${NUMBER_OF_QUESTIONS}`})
+      </h1>
 
-      <p>{JSON.stringify(questions)}</p>
+      {answerList}
 
-      <button
-        onClick={() => {
-          const currentQuestions = [...questions];
-
-          const questionsWithAnswers: IQuestionsWithAnswers =
-            currentQuestions.map((question) => {
-              return {
-                ...question,
-                currentAnswer: undefined,
-              };
-            });
-
-          setQuestions(questionsWithAnswers);
-
-          setCurrentQuestion(0);
-        }}
-      >
-        Play again
-      </button>
+      <button onClick={resetGame}>PLAY AGAIN?</button>
     </>
   );
 }
