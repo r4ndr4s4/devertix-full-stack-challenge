71fbb9bf57c324d66fbc1c613b294ae31e5f988d
András Rakita <Rakita.Andras@gmail.com>
2024-07-05 15:26:06 +0200

    fix(frontend): minor fixes in the intro component

--- frontend/src/Intro/index.tsx
+++ frontend/src/Intro/index.tsx
@@ -1,14 +1,22 @@
 import { useContext } from "react";
 import { AppStateContext } from "../App/AppState";
+import { NUMBER_OF_QUESTIONS } from "../App";
 
 function Intro() {
   const { setCurrentQuestion } = useContext(AppStateContext);
 
   return (
     <>
-      <h1>Intro</h1>
+      <h1>Welcome to the Trivia Challenge!</h1>
 
-      <button onClick={() => setCurrentQuestion(1)}>Begin</button>
+      <h2>
+        You will be presented with {NUMBER_OF_QUESTIONS} True of False
+        questions.
+      </h2>
+
+      <h2>Can you score 100%?</h2>
+
+      <button onClick={() => setCurrentQuestion(1)}>BEGIN</button>
     </>
   );
 }
